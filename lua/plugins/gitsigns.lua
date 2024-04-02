b71e8e1df251f7fc349b1ef2458f6cb23184c369
wai-lin <wailin.wilbert@gmail.com>
2024-04-02 08:12:23 +0630

    feat: gitsigns git blame keymap

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,9 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "VimEnter",
-	opts = {},
-}
+	config = function()
+		require("gitsigns").setup({})
 
+		vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { desc = "[G]itSigns: [B]lame line" })
+	end,
+}
