3b6db8f77f76c7fd315aa7e4827659c7bb224ea5
wai-lin <dev.wailin@gmail.com>
2025-08-10 22:48:44 +0700

    feat: configured theme and modded lualine

---
+++ nvim_config/lua/plugins/theme.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      style = "dark",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        separator_style = "thick",
+      },
+    },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "vscode",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+    },
+  },
+}
