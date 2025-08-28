02c3bd009c2b1fbc09798115015338f4474074f6
wai-lin <dev.wailin@gmail.com>
2025-08-28 17:44:58 +0700

    fix: blink.cmp not working

--- nvim_config/lua/plugins/languages_setup.lua
+++ nvim_config/lua/plugins/languages_setup.lua
@@ -34,24 +34,32 @@ return {
 	{
 		"saghen/blink.cmp",
 		version = "1.*",
-		dependencies = { "rafamadriz/friendly-snippets" },
+		enabled = true,
+		dependencies = {
+			{ "rafamadriz/friendly-snippets" },
+			{ "saghen/blink.compat", optional = true, opts = {} },
+		},
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
 			keymap = { preset = "default" },
-			appearance = {
-				nerd_font_variant = "mono",
-			},
 			completion = {
 				documentation = { auto_show = true },
 			},
 			sources = {
+				compact = {},
 				default = { "lsp", "path", "snippets", "buffer" },
+				-- providers = {
+				-- 	lsp = {
+				-- 		name = "lsp",
+				-- 		enabled = true,
+				-- 		module = "blink.cmp.sources.lsp",
+				-- 	},
+				-- },
 			},
-			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		opts_extends = { "sources.default" },
-		config = function()
+		config = function(_, opts)
 			local gen_loader = require("mini.snippets").gen_loader
 			require("mini.snippets").setup({
 				snippets = {
@@ -62,22 +70,30 @@ return {
 					gen_loader.from_lang(),
 				},
 			})
+			require("blink.cmp").setup(opts)
 		end,
 	},
 
 	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			{ "neovim/nvim-lspconfig" },
-		},
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		opts = {
-			automatic_enable = false,
+			auto_update = true,
 			ensure_installed = {
 				"lua_ls",
 				"eslint_d",
 				"ts_ls",
 			},
+		},
+	},
+
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{ "mason-org/mason.nvim", opts = {} },
+		},
+		opts = {
+			automatic_enable = true,
 			servers = {
 				lua_ls = {
 					settings = {
@@ -96,80 +112,6 @@ return {
 				vim.lsp.config(server, config)
 				vim.lsp.enable(server)
 			end
-
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
-				callback = function(event)
-					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-					-- to define small helper and utility functions so you don't have to repeat yourself.
-					--
-					-- In this case, we create a function that lets us more easily define mappings specific
-					-- for LSP related items. It sets the mode, buffer and description for us each time.
-					local map = function(keys, func, desc)
-						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-					end
-
-					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-					-- Jump to the implementation of the word under your cursor.
-					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-					-- Jump to the type of the word under your cursor.
-					--  Useful when you're not sure what type a variable is and you want to see
-					--  the definition of its *type*, not where it was *defined*.
-					map("gtd", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype [D]efinition")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
-					map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-					-- Execute a code action, usually your cursor needs to be on top of an error
-					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-					map("<leader>cd", vim.diagnostic.open_float, "Open [C]ode [D]iagnostic")
-
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-					-- The following code creates a keymap to toggle inlay hints in your
-					-- code, if the language server you are using supports them
-					--
-					-- This may be unwanted, since they displace some of your code
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-						end, "[T]oggle Inlay [H]ints")
-					end
-
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("UserLspDetach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-							end,
-						})
-					end
-				end,
-			})
 		end,
 	},
 }
