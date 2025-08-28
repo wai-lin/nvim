64cd88aac96b8316afe9b2085934250c93380a8e
wai-lin <dev.wailin@gmail.com>
2025-08-28 17:45:12 +0700

    feat: customized statusline

--- nvim_config/lua/plugins/mini.lua
+++ nvim_config/lua/plugins/mini.lua
@@ -1,6 +1,10 @@
 return {
 	{
 		"echasnovski/mini.nvim",
+		---@module 'mini'
+		---@type mini.SetupOpts
+		opts = {},
+
 		config = function()
 			require("mini.icons").setup()
 			require("mini.notify").setup()
@@ -16,10 +20,52 @@ return {
 			require("mini.extra").setup()
 			require("mini.git").setup()
 			require("mini.diff").setup()
-			require("mini.statusline").setup()
+
+			-- MiniStatusline
+			local MiniStatusline = require('mini.statusline')
+
+			local function location()
+			  local line_icon = ""  -- You can use any icon you like here (this is a common line icon)
+			  local col_icon = "󱥖"   -- Icon for column (choose any you prefer)
+
+			  local line = vim.fn.line(".")          -- current line number
+			  local col = vim.fn.col(".")            -- current column number
+
+			  -- Format: "icon line" and "icon col"
+			  local line_str = string.format("%s %d", line_icon, line)
+			  local col_str = string.format("%s %d", col_icon, col)
+
+			  -- Return combined string with a space between
+			  return line_str .. "  " .. col_str
+			end
+
+			require("mini.statusline").setup({
+				content = {
+					inactive = nil,
+					active = function()
+						local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+						local git           = MiniStatusline.section_git({ trunc_width = 75 })
+						local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+						local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+						local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+						local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+						-- local location      = MiniStatusline.section_location({ trunc_width = 75 })
+
+						-- Get current time in H:m format
+						local time = "  " .. os.date("%H:%M")
+
+						return MiniStatusline.combine_groups({
+							{ hl = mode_hl,                  strings = { mode } },
+							{ hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
+							'%<', -- Mark general truncate point
+							{ hl = 'MiniStatuslineFilename', strings = { filename } },
+							'%=', -- End left alignment
+							{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+							{ hl = mode_hl,                  strings = { search, location(), time } },
+						})
+					end,
+				},
+			})
 		end,
-		---@module 'mini'
-		---@type mini.SetupOpts
-		opts = {},
 	},
 }
