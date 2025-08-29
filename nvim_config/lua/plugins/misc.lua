8ed976da872e555bd6c0e023e34b6018bfa8d546
wai-lin <dev.wailin@gmail.com>
2025-08-29 11:18:40 +0700

    feat: todo comments plugin

--- nvim_config/lua/plugins/misc.lua
+++ nvim_config/lua/plugins/misc.lua
@@ -36,4 +36,31 @@ return {
 			vim.keymap.set("n", "<leader>zm", "<CMD>lua require('zen-mode').toggle({})<CR>", { desc = "[Z]en [M]ode" })
 		end,
 	},
+
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			gui_style = {
+				fg = "BOLD", -- The gui style to use for the fg highlight group.
+				bg = "BOLD", -- The gui style to use for the bg highlight group.
+			},
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+				info = { "DiagnosticInfo", "#2563EB" },
+				hint = { "DiagnosticHint", "#10B981" },
+				default = { "Identifier", "#7C3AED" },
+				test = { "Identifier", "#FF00FF" },
+			},
+		},
+		-- TODO: hello
+		-- TODO: hi
+		config = function(_, opts)
+			local todo_comments = require("todo-comments")
+			todo_comments.setup(opts)
+
+			vim.keymap.set("n", "<leader>ft", "<CMD>TodoLocList<CR><ESC>", { desc = "Search todo comments in project" })
+		end,
+	},
 }
