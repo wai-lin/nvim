bc4c45672adcb4369a32b85f71d30398fbc8462e
wai-lin <dev.wailin@gmail.com>
2025-08-29 12:51:50 +0700

    feat: keymap for lsp references

--- nvim_config/lua/plugins/telescope.lua
+++ nvim_config/lua/plugins/telescope.lua
@@ -20,6 +20,7 @@ return {
 		vim.keymap.set("n", "<leader>ff", builtin("find_files"), { desc = "Telescope: Find files" })
 		vim.keymap.set("n", "<leader>fg", builtin("live_grep"), { desc = "Telescope: Live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin("buffers"), { desc = "Telescope: Buffers" })
+		vim.keymap.set("n", "<leader>fr", builtin("lsp_references"), { desc = "Telescope: LSP References" })
 		vim.keymap.set("n", "<leader>fd", builtin("diagnostics"), { desc = "Telescope: LSP Diagnostics" })
 		vim.keymap.set("n", "<leader>fh", builtin("help_tags"), { desc = "Telescope: Help tags" })
 		vim.keymap.set("n", "<leader>fp", builtin("builtin"), { desc = "Telescope: List pickers" })
