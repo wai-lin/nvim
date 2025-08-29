4e4bba2d7af1f79fd6892536447a3ec488118edf
wai-lin <dev.wailin@gmail.com>
2025-08-29 09:42:49 +0700

    feat: change telescope to dropdown layout

--- nvim_config/lua/plugins/telescope.lua
+++ nvim_config/lua/plugins/telescope.lua
@@ -11,11 +11,17 @@ return {
 			},
 		})
 
-		local builtin = require("telescope.builtin")
+		local builtin = function(picker)
+			return "<CMD>lua require('telescope.builtin')."
+				.. picker
+				.. "(require('telescope.themes').get_dropdown({}))<CR><ESC>"
+		end
 
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
+		vim.keymap.set("n", "<leader>ff", builtin("find_files"), { desc = "Telescope: Find files" })
+		vim.keymap.set("n", "<leader>fg", builtin("live_grep"), { desc = "Telescope: Live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin("buffers"), { desc = "Telescope: Buffers" })
+		vim.keymap.set("n", "<leader>fd", builtin("diagnostics"), { desc = "Telescope: LSP Diagnostics" })
+		vim.keymap.set("n", "<leader>fh", builtin("help_tags"), { desc = "Telescope: Help tags" })
+		vim.keymap.set("n", "<leader>fp", builtin("builtin"), { desc = "Telescope: List pickers" })
 	end,
 }
