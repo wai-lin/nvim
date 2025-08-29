e7777533555bd73fca921b79a77f4915a2448dca
wai-lin <dev.wailin@gmail.com>
2025-08-29 09:42:25 +0700

    feat: add device battery section in statusline

--- nvim_config/lua/plugins/mini.lua
+++ nvim_config/lua/plugins/mini.lua
@@ -1,6 +1,15 @@
 return {
 	{
 		"echasnovski/mini.nvim",
+
+		dependencies = {
+			-- For MiniStatusline: Start
+			"justinhj/battery.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			-- For MiniStatusline: End
+		},
+
 		---@module 'mini'
 		---@type mini.SetupOpts
 		opts = {},
@@ -22,46 +31,48 @@ return {
 			require("mini.diff").setup()
 
 			-- MiniStatusline
-			local MiniStatusline = require('mini.statusline')
+			local MiniStatusline = require("mini.statusline")
 
-			local function location()
-			  local line_icon = ""  -- You can use any icon you like here (this is a common line icon)
-			  local col_icon = "󱥖"   -- Icon for column (choose any you prefer)
+			local function get_location()
+				local line_icon = "" -- You can use any icon you like here (this is a common line icon)
+				local col_icon = "󱥖" -- Icon for column (choose any you prefer)
 
-			  local line = vim.fn.line(".")          -- current line number
-			  local col = vim.fn.col(".")            -- current column number
+				local line = vim.fn.line(".") -- current line number
+				local col = vim.fn.col(".") -- current column number
 
-			  -- Format: "icon line" and "icon col"
-			  local line_str = string.format("%s %d", line_icon, line)
-			  local col_str = string.format("%s %d", col_icon, col)
+				-- Format: "icon line" and "icon col"
+				local line_str = string.format("%s %d", line_icon, line)
+				local col_str = string.format("%s %d", col_icon, col)
 
-			  -- Return combined string with a space between
-			  return line_str .. "  " .. col_str
+				-- Return combined string with a space between
+				return line_str .. "  " .. col_str
 			end
+
+			local battery = require("battery")
+			battery.setup({ update_rate_seconds = 30 })
 
 			require("mini.statusline").setup({
 				content = {
 					inactive = nil,
 					active = function()
 						local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-						local git           = MiniStatusline.section_git({ trunc_width = 75 })
-						local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-						local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-						local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-						local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
-						-- local location      = MiniStatusline.section_location({ trunc_width = 75 })
-
-						-- Get current time in H:m format
+						local git = MiniStatusline.section_git({ trunc_width = 75 })
+						local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+						local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+						local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+						local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+						local location = get_location()
+						local battery_status = " " .. battery.get_status_line()
 						local time = "  " .. os.date("%H:%M")
 
 						return MiniStatusline.combine_groups({
-							{ hl = mode_hl,                  strings = { mode } },
-							{ hl = 'MiniStatuslineDevinfo',  strings = { git, diagnostics } },
-							'%<', -- Mark general truncate point
-							{ hl = 'MiniStatuslineFilename', strings = { filename } },
-							'%=', -- End left alignment
-							{ hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-							{ hl = mode_hl,                  strings = { search, location(), time } },
+							{ hl = mode_hl, strings = { mode } },
+							{ hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
+							"%<", -- Mark general truncate point
+							{ hl = "MiniStatuslineFilename", strings = { filename } },
+							"%=", -- End left alignment
+							{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+							{ hl = mode_hl, strings = { search, location, battery_status, time } },
 						})
 					end,
 				},
