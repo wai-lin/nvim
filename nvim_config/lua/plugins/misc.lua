51856c427d0ee5376267512147a6b26b62b90337
wai-lin <dev.wailin@gmail.com>
2025-08-31 18:16:09 +0700

    feat: folke flash.nvim installed

--- nvim_config/lua/plugins/misc.lua
+++ nvim_config/lua/plugins/misc.lua
@@ -54,13 +54,26 @@ return {
 				test = { "Identifier", "#FF00FF" },
 			},
 		},
-		-- TODO: hello
-		-- TODO: hi
 		config = function(_, opts)
 			local todo_comments = require("todo-comments")
 			todo_comments.setup(opts)
 
 			vim.keymap.set("n", "<leader>ft", "<CMD>TodoLocList<CR><ESC>", { desc = "Search todo comments in project" })
 		end,
+	},
+
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "<m-f>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "<c-f>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		},
 	},
 }
