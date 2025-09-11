f125a6b1b4ac60f7b1d535ced7de07aa5a097ae2
wai-lin <dev.wailin@gmail.com>
2025-09-11 15:39:47 +0700

    feat: gruvbox hard contrast theme

--- nvim_config/lua/plugins/theme.lua
+++ nvim_config/lua/plugins/theme.lua
@@ -1,7 +1,20 @@
 return {
-	"rebelot/kanagawa.nvim",
-	opts = {},
-	config = function()
-		vim.cmd([[colorscheme kanagawa-dragon]])
-	end,
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard",
+			})
+			vim.cmd.colorscheme("gruvbox")
+			vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+		end,
+	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		vim.cmd([[colorscheme kanagawa-dragon]])
+	-- 	end,
+	-- },
 }
